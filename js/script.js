231a7fd6008cf99b657d51467eb47c84f60d715f
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-07-21 22:36:18 +0500

    update button deg

--- js/script.js
+++ js/script.js
@@ -26,12 +26,31 @@ filterElement.forEach(option => {  // buttonlani bosganda xammasini kok qildik
         document.querySelector(".filter .active").classList.remove("active")
         option.classList.add("active")
         filterNameElement.textContent = option.textContent
+
+        if(option.id === "brightness") {
+            filterSliderElement.max = "200"
+            filterSliderElement.value = brightness
+            filterValueElement.textContent = `${brightness}%`
+        } else if (option.id === "saturation") {
+            filterSliderElement.max = "200"
+            filterSliderElement.value = saturation
+            filterValueElement.textContent = `${saturation}%`
+        } else if (option.id === "inversion") {
+            filterSliderElement.max = "100"
+            filterSliderElement.value = inversion
+            filterValueElement.textContent = `${inversion}%`
+        } else  {
+            filterSliderElement.max = "100"
+            filterSliderElement.value = grayscale
+            filterValueElement.textContent = `${grayscale}%`
+        }
     })
 })
 
 const updateFilter = () => {
     filterValueElement.textContent = `${filterSliderElement.value}%`
     const selectedFilterElement = document.querySelector(".filter .active")
+    
     if(selectedFilterElement.id === "brightness") {
         brightness = filterSliderElement.value
     } else if(selectedFilterElement.id === "saturation") {
