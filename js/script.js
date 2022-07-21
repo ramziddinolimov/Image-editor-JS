5bb0112e78aa55f6d1a3adbe06bbb6b7a1a9eba7
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-07-21 22:12:27 +0500

    brightnss started

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@ const chooseImgBtnElement = document.querySelector(".choose-img")
 const previewImgElement = document.querySelector(".preview-img img")
 const filterElement = document.querySelectorAll(".filter button")
 const filterNameElement = document.querySelector(".filter-info .name")
-const filterSliderElement = document.querySelector(".filter-info input")
+const filterSliderElement = document.querySelector(".slider input")
+const filterValueElement = document.querySelector(".filter-info .value")
+
+
+
+let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
 
 const loadImage = () => {
     let file = fileInputElement.files[0]
@@ -20,8 +25,14 @@ filterElement.forEach(option => {  // buttonlani bosganda xammasini kok qildik
     option.addEventListener("click", () => {
         document.querySelector(".filter .active").classList.remove("active")
         option.classList.add("active")
+        filterNameElement.textContent = option.textContent
     })
 })
 
+const updateFilter = () => {
+    filterValueElement.textContent = `${filterSliderElement.value}%`
+}
+
 fileInputElement.addEventListener("change", loadImage)
 chooseImgBtnElement.addEventListener('click', () => fileInputElement.click())
+filterSliderElement.addEventListener("input", updateFilter)
