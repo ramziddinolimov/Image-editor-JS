37573cca6669a6d99a9e4ae363cc6e41ba930150
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-07-21 21:38:47 +0500

    container disabled

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 const fileInputElement = document.querySelector(".file-input")
 const chooseImgBtnElement = document.querySelector(".choose-img")
 const previewImgElement = document.querySelector(".preview-img img")
+const filterElement = document.querySelectorAll(".filter button")
 
 const loadImage = () => {
     let file = fileInputElement.files[0]
     if(!file) return
-    previewImgElement.src = URL.createObjectURL(file)
+    previewImgElement.src = URL.createObjectURL(file) // rasm qoshdik
     console.log(file);
+    previewImgElement.addEventListener("load", () => {
+        document.querySelector(".container").classList.remove("disable")
+        // xammasini disabled qldik
+    })
 }
+
+filterElement.forEach(option => {
+    option.addEventListener("click", () => {
+        document.querySelector(".filter .active").classList.remove("active")
+        option.classList.add("active")
+    })
+})
 
 fileInputElement.addEventListener("change", loadImage)
 chooseImgBtnElement.addEventListener('click', () => fileInputElement.click())
